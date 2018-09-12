40618c96bd4304ad81f53344776e393e2c05a9cc
Brenno Passanha <brenno.passanha@scout24.com>
2018-09-12 16:30:59 +0200

    Fixing issues

--- entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# As cron runs without any environment variable, this command will import all environment
-# variables file and run it on every cronjob run
-printenv | sed 's/^\(.*\)$/export \1/g' > /root/env_vars.sh
-chmod +x /root/env_vars.sh
--- scripts/tools/cron_log.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source /root/env_vars.sh
-
-echo "$(date): executed cron" >> /var/log/scripts/cron.log
